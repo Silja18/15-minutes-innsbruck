3ea8dcbf3547834435fc76f0bffbb8a92cfbd719
Silja18 <silja.baumann@student.uibk.ac.at>
2021-10-18 20:07:45 +0200

    leaflet in html eingebunden, Karte in main.js einebunden

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const map = L.map("map", {
+// Kartenhintergründe der basemap.at definieren
+let baselayers = {
+    standard: L.tileLayer.provider("BasemapAT.basemap"),
+    terrain: L.tileLayer.provider("BasemapAT.terrain"),
+    ortho_overlay: L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
+};
+// // Overlays für die Themen zum Ein- und Ausschalten definieren
+let overlays = {
+    busLines: L.featureGroup(),
+    busStops: L.markerClusterGroup(),
+    pedAreas: L.featureGroup(),
+    sights: L.featureGroup()
+};
+
+// Karte initialisieren und auf Innsbruck Wikipedia Koordinaten blicken
+let map = L.map("map", {
     fullscreenControl: true,
-    center: [ stop.lat, stop.lng ], 
+    center: [47.267222, 11.392778],
     zoom: 13,
     layers: [
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+        baselayers.grau
     ]
 });
