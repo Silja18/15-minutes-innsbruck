382ace07295041ef6af9809e0a4772a6bba12691
Silja18 <silja.baumann@student.uibk.ac.at>
2021-10-18 22:44:32 +0200

    daseinsvorsorge geojson daten eingebunden

--- colors.js
+++ colors.js
@@ -1,12 +1,4 @@
 const COLORS = {
-    buslines: {
-        "Blue Line": "#0074D9",
-        "Green Line": "#2ECC40",
-        "Grey Line": "#AAAAAA",
-        "Orange Line": "#FF851B",
-        "Red Line": "#FF4136",
-        "Yellow Line": "#FFDC00"
-    },
     minutes: {
         "5": "#1a9850",
         "10": "#91cf60",
---
+++ ogdtirol.js
@@ -0,0 +1,7 @@
+// OGD tirol Daten
+// Quelle: https://data-tiris.opendata.arcgis.com/search?tags=US 
+const OGDTIROL = [{
+    title: "Daseinsvorsorge Tirol",
+    source: "https://data-tiris.opendata.arcgis.com/search?tags=US",
+    data: "data/Daseinsvorsorge.geojson"
+}];
