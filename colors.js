ec1d6dd775593305dcfc02aa558917191964dbac
Silja18 <silja.baumann@student.uibk.ac.at>
2021-10-18 22:24:10 +0200

    colors,js eingefügt

---
+++ colors.js
@@ -0,0 +1,26 @@
+const COLORS = {
+    buslines: {
+        "Blue Line": "#0074D9",
+        "Green Line": "#2ECC40",
+        "Grey Line": "#AAAAAA",
+        "Orange Line": "#FF851B",
+        "Red Line": "#FF4136",
+        "Yellow Line": "#FFDC00"
+    },
+    minutes: {
+        "5": "#1a9850",
+        "10": "#91cf60",
+        "15": "#d9ef8b",
+        "20": "#fee08b",
+        "25": "#fc8d59",
+        "30": "#d73027"
+    },
+    kilometers: {
+        "0.5": "#1a9850",
+        "1": "#91cf60",
+        "1.5": "#d9ef8b",
+        "2": "#fee08b",
+        "2.5": "#fc8d59",
+        "3": "#d73027"
+    }
+};
