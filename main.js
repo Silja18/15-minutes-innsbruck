680812795b7ed094a6ccd628c83c450494a5b5c6
Silja18 <silja.baumann@student.uibk.ac.at>
2021-10-18 22:23:35 +0200

    hash, reachability und colors eingefügt

--- main.js
+++ main.js
@@ -5,3 +5,45 @@ let map = L.map(`map`, {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     ]
 })
+// Leaflet hash einfügen
+new L.Hash(map);
+
+//Reachability plugin und Intervalle stylen
+let styleIntervals = (feature) => {
+    let color = "";
+    let range = feature.properties.Range;
+    if (feature.properties.Measure === "time") {
+        color = COLORS.minutes[range];
+    } else if (feature.properties.Measure === "distance") {
+        color = COLORS.kilometers[range];
+    } else {
+        color = "black";
+    }
+    return {
+        color: color,
+        opacity: 0.5,
+        fillOpacity: 0.2
+    }; 
+};
+
+L.control.reachability({
+    // add settings/options here
+    apiKey: '5b3ce3597851110001cf6248b8d6b6ede5124788bbc04a5ceb51108f',
+    styleFn: styleIntervals,
+    drawButtonContent: '',
+    drawButtonStyleClass: 'fa fa-pencil-alt fa-2x',
+    deleteButtonContent: '',
+    deleteButtonStyleClass: 'fa fa-trash fa-2x',
+    distanceButtonContent: '',
+    distanceButtonStyleClass: 'fa fa-road fa-2x',
+    timeButtonContent: '',
+    timeButtonStyleClass: 'fa fa-clock fa-2x',
+    travelModeButton1Content: '',
+    travelModeButton1StyleClass: 'fa fa-car fa-2x',
+    travelModeButton2Content: '',
+    travelModeButton2StyleClass: 'fa fa-bicycle fa-2x',
+    travelModeButton3Content: '',
+    travelModeButton3StyleClass: 'fa fa-male fa-2x',
+    travelModeButton4Content: '',
+    travelModeButton4StyleClass: 'fa fa-wheelchair fa-2x'
+}).addTo(map);
