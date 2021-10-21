c74a6a792b41b68760c94d77c360474e226f6ab4
Silja18 <silja.baumann@student.uibk.ac.at>
2021-10-21 13:57:38 +0200

    overlays zunächst wieder entfernt

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-// Kartenhintergründe der basemap.at definieren
-let baselayers = {
-    standard: L.tileLayer.provider("BasemapAT.basemap"),
-    grau: L.tileLayer.provider("BasemapAT.grau"),
-    terrain: L.tileLayer.provider("BasemapAT.terrain"),
-    surface: L.tileLayer.provider("BasemapAT.surface"),
-    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
-    ortho_overlay: L.layerGroup([
-        L.tileLayer.provider("BasemapAT.orthofoto"),
-        L.tileLayer.provider("BasemapAT.overlay")
-    ]),
-};
+
 
 // Overlay für Punkte zur Daseinsvorsorge
 let overlays = {
@@ -21,24 +10,14 @@ let map = L.map(`map`, {
     center: [47.267222, 11.392778],
     zoom: 12,
     layers: [
-        baselayers.grau
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     ]
 })
 
 // Overlay zur Layer Control hinzufügen
-
-// Kartenhintergründe und Overlays zur Layer-Control hinzufügen
 let layerControl = L.control.layers({
-    "basemap.at Standard": baselayers.standard,
-    "basemap.at grau": baselayers.grau,
-    "basemap.at Relief": baselayers.terrain,
-    "basemap.at Oberfläche": baselayers.surface,
-    "basemap.at hochauflösend": baselayers.highdpi,
-    "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
-}, {
     "Daseinsvorsorge": overlays.dasein,
 }).addTo(map);
-
 
 // Overlays nach dem Laden anzeigen
 overlays.dasein.addTo(map);
