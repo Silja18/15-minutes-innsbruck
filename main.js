5512a5474ebd7272ada4c1e03745cb197591744b
Silja18 <silja.baumann@student.uibk.ac.at>
2021-10-29 10:34:12 +0200

    baselayer auf grau geändert

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ let map = L.map(`map`, {
     center: [47.267222, 11.392778],
     zoom: 12,
     layers: [
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        baselayers.grau
     ]
 })
 
