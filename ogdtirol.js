115aaab07408c9ffcfed819d46a634c39da80991
Silja18 <silja.baumann@student.uibk.ac.at>
2021-10-31 14:09:32 +0100

    ogd tirol gelöscht

--- ogdtirol.js
+++
@@ -1,7 +0,0 @@
-// OGD tirol Daten
-// Quelle: https://data-tiris.opendata.arcgis.com/search?tags=US 
-const OGDTIROL = [{
-    title: "Daseinsvorsorge Tirol",
-    source: "https://data-tiris.opendata.arcgis.com/search?tags=US",
-    data: "data/Daseinsvorsorge.json"
-}];
