061899e417836be930423d4d9da6e2fb4577789e
Silja18 <silja.baumann@student.uibk.ac.at>
2022-07-12 17:32:07 +0200

    Gänsefüßchen bei lat und long entfernt

--- wish.js
+++ wish.js
@@ -1,32 +1,32 @@
 const WISH = [{
         "stadtteil": "Hötting",
         "wish": "2-3 zusätzliche Lokale (Cafe/Bar/Restaurant) würden Hötting sehr gut tun, da mittlerweile doch viele Menschen dort wohnen und für sämtliche Freizeitaktivitäten (ausgenommen Bergsport) und Erledigungen in die Stadt müssen. (431)",
-        "lat": "47.268228",
-        "lon": "11.372497",
+        "lat": 47.268228,
+        "lon": 11.372497,
     },
     {
         "stadtteil": "Höttinger Au",
         "wish": "Eine größere (in dem Fall breitere) Grünfläche - der Abschnitt am Inn ist recht schmal. (399)",
-        "lat": "47.253513",
-        "lon": "11.366770",
+        "lat": 47.253513,
+        "lon": 11.366770,
     },
     {
         "stadtteil": "Hötinger Au",
         "wish": "Mehr grün (zb. Bushalte Höttinger Au / West)",
-        "lat": "47.264895323811125",
-        "lon": "11.373003706058991",
+        "lat": 47.264895323811125,
+        "lon": 11.373003706058991,
     },
     {
         "stadtteil": "Höttinger Au",
         "wish": "Mitterweg müsste Charakter einer Wohnstraße erhalten, weniger Asphalt",
-        "lat": "47.25876830413813",
-        "lon": "11.373784831266851",
+        "lat": 47.25876830413813,
+        "lon": 11.373784831266851,
     },
     {
         "stadtteil": "Höttinger Au",
         "wish": "Gerade das Angebot von 'Wohlfühlorten', wie Cafés, schöne Ausblicke, nette Häuser, gibt es hier eigentlich nicht (Höttinger Au, Höhe Unterbergerstraße)",
-        "lat": "47.26310106217448",
-        "lon": "11.377368131738642",
+        "lat": 47.26310106217448,
+        "lon": 11.377368131738642,
     },
 
     {
