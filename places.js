33f9a77418b5cf7fa4f73c2509b372c3f19fd964
Silja18 <silja.baumann@student.uibk.ac.at>
2022-07-12 16:06:13 +0200

    überflüssige baselayer entfernt

--- places.js
+++ places.js
@@ -1,9 +1,5 @@
 // Kartenhintergründe der basemap.at definieren
 let baselayers = {
-    standard: L.tileLayer.provider("BasemapAT.basemap"),
-    grau: L.tileLayer.provider("BasemapAT.grau"),
-    terrain: L.tileLayer.provider("BasemapAT.terrain"),
-    surface: L.tileLayer.provider("BasemapAT.surface"),
     highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
     ortho_overlay: L.layerGroup([
         L.tileLayer.provider("BasemapAT.orthofoto"),
@@ -22,10 +18,6 @@ let map = L.map("map", {
 
 // Kartenhintergründe und Overlays zur Layer-Control hinzufügen
 let layerControl = L.control.layers({
-    "basemap.at Standard": baselayers.standard,
-    "basemap.at grau": baselayers.grau,
-    "basemap.at Relief": baselayers.terrain,
-    "basemap.at Oberfläche": baselayers.surface,
     "basemap.at hochauflösend": baselayers.highdpi,
     "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
 },).addTo(map);
