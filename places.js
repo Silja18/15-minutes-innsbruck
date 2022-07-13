febfe6a928090863bdae16b5e614bd6a64ab8778
Silja18 <silja.baumann@student.uibk.ac.at>
2022-07-13 10:20:30 +0200

    kleine Änderung

--- places.js
+++ places.js
@@ -7,9 +7,6 @@ let baselayers = {
     ]),
 };
 
-let overlays = {
-    wish: L.featureGroup()
-};
 
 // Karte initialisieren
 let map = L.map("map", {
@@ -24,68 +21,24 @@ let map = L.map("map", {
 let layerControl = L.control.layers({
     "basemap.at hochauflösend": baselayers.highdpi,
     "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
-}, {
-    "Wünsche und Vorschläge": overlays.wish,
-}
+}, 
 )
 .addTo(map);
 
-overlays.wish.addTo(map);
-
-/* let drawWish = (wish) => {
-    L.geoJson(wish, {
-        onEachFeature: (feature, layer) => {
-            layer.bindPopup(`<strong>${feature.properties.stadtteil}</strong>
-            <hr>
-            Wunsch: ${feature.properties.wish}`)
-        },
-        pointToLayer: (geoJsonPoint, latlng) => {
-            return L.marker(latlng, {
-                icon: L.icon({
-                    iconUrl: 'images/icons/welfareroom.png',
-                    iconSize: [35, 35]
-                })
-            })
-        }
-    }).addTo(overlays.wish);
-}
-*/
 L.geoJSON(WISH).addTo(map);
-// 
+
 function onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.STADTTEIL) {
-    layer.bindPopup(`
-    <h2>Stadtteil: ${feature.properties.STADTTEIL}
-    <h3>Wunsch: ${feature.properties.WISH} 
-    `)}
+    if (feature.properties && feature.properties.stadtteil) {
+        layer.bindPopup(`
+        <h2>Stadtteil: ${feature.properties.stadtteil}</h2>
+        <p>Vorschlag: ${feature.properties.wish}</p>
+        `);
+    }
 }
 
 L.geoJSON(WISH, {
     onEachFeature: onEachFeature
 }).addTo(map);
-
-
-/* var marker = (function () {
-    for (let index = 0; index < WISH.length; index++) {
-        let marker = L.marker([WISH[index].lat, WISH[index].lon],
-            )
-    marker.bindPopup(`
-    <h2>${WISH[index].stadtteil}</h2>
-    <p>${WISH[index].wish}</p>
-    `)
-    .addTo(map)        
-    }
-})
-*/
-
-
-/* let marker = L.marker([WISH.lat, WISH.long])
-marker.bindPopup(`
-    <h2>${WISH[index].stadtteil}</h2>
-    <p>${WISH[index].wish}</p>
-    `)
-    .addTo(map) 
-*/
 
 
 // Leaflet hash einfügen
