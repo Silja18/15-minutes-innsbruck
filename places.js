ddd143e2ea984276de5ee597fd6faad74ad379fd
Silja18 <silja.baumann@student.uibk.ac.at>
2022-07-22 10:05:17 +0200

    OSM Attribution

--- places.js
+++ places.js
@@ -13,7 +13,9 @@ let map = L.map("map", {
     center: [47.267222, 11.392778],
     zoom: 12,
     layers: [
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'  
+        })
     ]
 });
 
